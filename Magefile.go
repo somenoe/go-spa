6850cabc1c44e5e3c0772f00665a8776561b295f
somenoe <otashi24@gmail.com>
2025-01-03 23:39:28 +0700

    chore: Migrate build system from make to mage

---
+++ Magefile.go
@@ -0,0 +1,18 @@
+//go:build mage
+
+package main
+
+import (
+	"github.com/magefile/mage/sh"
+)
+
+// Runs go mod download and then installs the binary.
+func Build() error {
+	if err := sh.RunWith(map[string]string{
+		"GOARCH": "wasm",
+		"GOOS":   "js",
+	}, "go", "build", "-o", "dist/web/app.wasm"); err != nil {
+		return err
+	}
+	return sh.Run("go", "build", "-o", "tmp/bin/main.exe")
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.23.4
 
 require github.com/maxence-charriere/go-app/v10 v10.0.9
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/magefile/mage v1.15.0 // indirect
+)
