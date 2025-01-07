6ed5020ff7f0f3511188843162e1b0c721b263cc
somenoe <otashi24@gmail.com>
2025-01-07 23:49:28 +0700

    feat: Add build time logging to mage targets

--- Magefile.go
+++ Magefile.go
@@ -3,19 +3,62 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
 
-func Wasm() error {
-	return sh.RunWith(map[string]string{
+const (
+	// DIFF_FORMATTER = "04:05.000"
+	DIFF_FORMATTER = "05.000"
+)
+
+func Wasm() {
+	startTime := time.Now()
+	sh.RunWith(map[string]string{
 		"GOARCH": "wasm",
 		"GOOS":   "js",
 	}, "go", "build", "-o", "dist/web/app.wasm")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
+
+	fmt.Println("wasm\t=>", formattedDiff)
 }
 
-func Build() error {
+func Build() {
 	mg.Deps(Wasm)
-	return sh.Run("go", "build", "-o", "tmp/bin/main.exe")
-	// return sh.Run("go", "run",".")
+	startTime := time.Now()
+	sh.Run("go", "build", "-o", "tmp/main.exe")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
+
+	fmt.Println("build\t=>", formattedDiff)
+}
+
+func Run() {
+	mg.Deps(Build)
+
+	startTime := time.Now()
+	sh.Run("tmp/main.exe")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
+
+	fmt.Println("run\t=>", formattedDiff)
+}
+
+func Dev() {
+	mg.Deps(Wasm)
+
+	startTime := time.Now()
+	sh.Run("go", "run", ".")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
+
+	fmt.Println("dev\t=>", formattedDiff)
 }
